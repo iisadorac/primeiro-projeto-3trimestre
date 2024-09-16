33a0fb7c2e1d6495b0b3e3cc5622f0d4bd73e9f7
Isadora <137515981+iisadorac@users.noreply.github.com>
2024-09-16 20:40:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    '1- Informação ',
+    'Quem sou eu?',
+    'Me chamo Isadora, tenho 16 anos e sou estudante do Ensino Médio.'
+)
+
+criaCartao(
+    '2- Informação',
+    'Como é a minha família?',
+    'É composta por 4 pessoas, eu, meu pai, minha mãe e meu irmão.'
+)
+
+criaCartao(
+    '3- Informação',
+    'Onde eu moro?',
+    'Sou residente da cidade de Pinhão, no Paraná. A latitude e longitude são -25.701501489810063, -51.648651152599875.'
+)
+criaCartao(
+    '4- Informação',
+    'Onde eu estudo?',
+    'No Colégio Estadual Professor Mario Evaldo Morski, na segunda série do ensino médio. O Colégio Morski foi fundado em 1979 em Pinhão, uma cidade no interior do Paraná. '
+)
+criaCartao(
+    '5- Informação',
+    'Como é a minha turma?',
+    'É a turma focada em exatas, uma turma grande, com 30 estudantes. Não sou amiga nem da metade da turma, apenas de algumas meninas.'
+)
+
+criaCartao(
+    '6- Informação',
+    'Qual a faculdade que pretendo fazer?',
+    'A faculdade de Direito, para futuramente me tornar uma juíza. Meu sonho é cursar uma universidade federal.'
+)
+
+criaCartao(
+    '7- Informação',
+    'Como é minha cidade?',
+    'É um lugar um pouco ruim para se viver, não há opções de lazer. Não é modernizado, e muitas vezes se encontra sujo.'
+)
+
+criaCartao(
+    '8- Informação',
+    'Qual é o meu sonho?',
+    'Quero crescer, cursar a faculdade dos meus sonhos, ter estabilidade financeira. Viajar ao mundo e ter minha própria casa. Quero casar com o meu namorado e ter um filho menino. Viver bem. '
+)
